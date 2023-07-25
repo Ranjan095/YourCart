f84bafb3548e1711aea6540e9522734d2963f39f
Ranjan Yadav <113471389+Ranjan095@users.noreply.github.com>
2023-07-25 12:47:28 +0530

    implementing MongoDB in this project

--- ReactApp_with_json_file/src/Redux/store.js
+++ ReactApp_with_json_file/src/Redux/store.js
@@ -1,8 +1,8 @@
 /** @format */
 
 import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
-import shoesReducer from "./Shoes/shoesReducer";
 import thunk from "redux-thunk";
+import shoesReducer from "./Shoes/shoesReducer";
 import authReducer from "./Authantication/authReducer";
 import cartReducer from "./Cart/cartReducer";
 let rootReducer = combineReducers({
